9e5371d61c13cce14952f3e4d8a887e511225b41
ZhenyuLi <893652269@qq.com>
2025-02-17 16:15:26 -0500

    fix: zsh plugin

--- install.sh
+++ install.sh
@@ -82,5 +82,6 @@ echo ">>>>>>>>Installing nvim<<<<<<<<"
 mkdir -p $HOME/.config
 cp -r nvim $HOME/.config
 install_nvim
+sed -i 's/plugins=(git)/plugins=(\n    git\n    z\n    zsh-autosuggestions\n    zsh-syntax-highlighting\n)/' ~/.zshrc
 
 
